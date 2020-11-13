866b0453171f1979f204a2a210a0838c8985ecef
Rebecca Mead <beckyleemead@gmail.com>
2020-11-13 07:56:48 -0500

    user database seeding

--- database/seeders/BookSeeder.php
+++ database/seeders/BookSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class BookSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -13,9 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([
-            UserSeeder::class,
-            // BookSeeder::class
-        ]);
+        //
     }
 }
